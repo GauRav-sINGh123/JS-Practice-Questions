3b0aa68e390ceb8307d0ac5419f5bf23284d6bc1
Gaurav Singh <gksingh947@gmail.com>
2024-11-20 22:39:46 +0530

    Guess the output more questions

---
+++ output/typecoercion/script.js
@@ -0,0 +1,39 @@
+console.log(5+"5"-1); // Guess the ouput
+
+console.log("10" * 2); // Guess the output
+
+console.log(15 / "3"); // Guess the output
+
+console.log("5" + 3 - 2); // Guess the output
+
+console.log(5 + 2 + "3"); // Guess the output
+
+console.log("10" - 5 + "2"); // Guess the output
+
+console.log("5" + true); // Guess the output
+
+console.log(5 * null); // Guess the output
+
+console.log(null + "5"); // Guess the output
+
+console.log(undefined + "5"); // Guess the output
+
+console.log(undefined + 1); // Guess the output
+
+console.log("15" - "5" + "2"); // Guess the output
+
+console.log(1 + +"5" - 3); // Guess the output
+
+console.log("5" - -3); // Guess the output
+
+console.log("5" * "2" + "3"); // Guess the output
+
+console.log("10" / "2" - 1); // Guess the output
+
+console.log(true + 1 + "5"); // Guess the output
+
+console.log(false - 3 + "7"); // Guess the output
+ 
+console.log("7" + 2 * 3); // Guess the output
+
+console.log(10 - "5" + "3"); // Guess the output
